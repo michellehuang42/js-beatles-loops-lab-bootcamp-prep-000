b5c48c36f0ef4498d9e6949698183698fee6c07a
MichelleHuang Huang <mh3793@stern.nyu.edu>
2018-03-08 18:17:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function theBeatlesPlay (musicians,instruments) {
+  var array = [];
+  for (let i=0; i<musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array;
+}
+
+function johnLennonFacts (facts) {
+  var array = [];
+  let i = 0;
+  while (i < facts.length) {
+    array.push(facts[i] + "!!!")
+    i++
+  }
+  return array;
+}
+
+function iLoveTheBeatles (number) {
+  var array = [];
+  let i = 0;
+  
+  function maybeTrue() {
+    return (i < 15 && i < number)
+  }
+  
+  do {
+    array.push("I love the Beatles!")
+    i++;
+  } while (maybeTrue());
+  return array;
+}
